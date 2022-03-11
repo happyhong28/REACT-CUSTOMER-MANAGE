90b6a78b20368d30e2750b90daeeaf112c480142
Dahye Hong <happyhong28@naver.com>
2022-03-12 01:46:07 +0900

    Material UI를 적용해서 고객 테이블 디자인하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import "./App.css";
 import React, { Component } from "react";
 import Customer from "./components/Customer";
+import Paper from "@mui/material/Paper"; //어떠한 컴포넌트의 외부를 감싸기 위한 컴포넌트 중 하나이다.
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { withStyles } from "@mui/styles";
+
+
+const styles = {
+  root: {
+    width: "100%",
+    overflowX: "auto",
+    marginTop : 50
+  },
+  table: {
+    minWidth: 1080,
+  },
+};
 
 const customers = [
   {
@@ -15,15 +34,15 @@ const customers = [
     id: 2,
     image: "https://placeimg.com/64/64/2",
     name: "김영훈",
-    birthday: "990208",
+    birthday: "970208",
     gender: "여자",
     job: "대학생",
   },
   {
     id: 3,
     image: "https://placeimg.com/64/64/3",
-    name: "김선우",
-    birthday: "990208",
+    name: "문형서",
+    birthday: "980208",
     gender: "여자",
     job: "대학생",
   },
@@ -31,24 +50,41 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {customers.map((c) => {
-          return (
-            <Customer
-              key={c.id} //map 함수를 사용할 때는 꼭 키값을 넣어주어야한다.
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })}
-      </div>
-    );
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c) => {
+            return (
+              <Customer
+                key={c.id} //map 함수를 사용할 때는 꼭 키값을 넣어주어야한다.
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+
+
+);
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
